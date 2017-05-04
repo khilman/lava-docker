c54fe8c06a7194677a3b7d908f499b84fd677933
Kevin Hilman <khilman@baylibre.com>
2017-05-04 10:34:24 +0200

    scripts: auto-add boards based on device-type

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -15,43 +15,18 @@ else
 fi
 
 # By default add a worker on the master
-lava-server manage workers add $(hostname)
+#lava-server manage workers add $(hostname)
 
 # Add devices on master
-lava-server manage device-types add qemu
-lava-server manage devices add --device-type qemu --worker $(hostname) qemu-01
-lava-server manage device-dictionary --hostname qemu-01 --import /etc/dispatcher-config/devices/qemu-device-dictionary.jinja2
-lava-server manage devices add --device-type qemu --worker $(hostname) qemu-02
-lava-server manage device-dictionary --hostname qemu-02 --import /etc/dispatcher-config/devices/qemu-device-dictionary.jinja2
+#lava-server manage device-types add qemu
+#lava-server manage devices add --device-type qemu --worker $(hostname) qemu-01
+#lava-server manage device-dictionary --hostname qemu-01 --import /etc/dispatcher-config/devices/qemu-device-dictionary.jinja2
+#lava-server manage devices add --device-type qemu --worker $(hostname) qemu-02
+#lava-server manage device-dictionary --hostname qemu-02 --import /etc/dispatcher-config/devices/qemu-device-dictionary.jinja2
 
 # add remote workers
 SLAVE=lab-slave-0
-lava-server manage pipeline-worker --hostname $SLAVE
+lava-server manage workers add $SLAVE
 
 # add remote devices
-lava-server manage devices add --device-type qemu --worker $SLAVE qemu-03
-lava-server manage device-dictionary --hostname qemu-03 --import /etc/dispatcher-config/devices/qemu-device-dictionary.jinja2
-lava-server manage devices add --device-type qemu --worker $SLAVE qemu-04
-lava-server manage device-dictionary --hostname qemu-04 --import /etc/dispatcher-config/devices/qemu-device-dictionary.jinja2
-
-# Array of <board name>:<LAVA device-type>
-#lava-server manage device-types add beaglebone-black
-#lava-server manage devices add --device-type beaglebone-black --worker $SLAVE am335x-boneblack
-#lava-server manage device-dictionary --hostname am335x-boneblack --import /etc/dispatcher-config/devices/am335x-boneblack.jinja2
-#exit 0
-
-#BOARDS="am335x-boneblack:beaglebone-black"
-BOARDS="am335x-boneblack:beaglebone-black omap4-panda-es:panda"
-#BOARDS="am335x-boneblack:beaglebone-black omap4-panda-es:panda meson8b-odroidc1-0:meson8b-odroidc1"
-
-
-for board_type in ${BOARDS}; do
-  IFS=':' read -a arr <<< "$board_type"
-  board_name=${arr[0]}
-  device_type=${arr[1]}
-  IFS=$OIFS
-
-  lava-server manage device-types add ${device_type}
-  lava-server manage devices add --device-type ${device_type} --worker $SLAVE ${board_name}
-  lava-server manage device-dictionary --hostname ${board_name} --import /etc/dispatcher-config/devices/${board_name}.jinja2
-done
+/add-boards.py --slave $SLAVE
