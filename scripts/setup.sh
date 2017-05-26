8cceb59eb4efc33e4ad0282ab975336f55eb2e7f
Kevin Hilman <khilman@baylibre.com>
2017-05-27 01:33:00 +0200

    add user/token from tokens/ dir

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,25 +3,22 @@
 if [[ -n "$LAVA_SERVER_IP" ]]; then
 	sed -i "s/.*LAVA_SERVER_IP =.*/LAVA_SERVER_IP = $LAVA_SERVER_IP/g" /etc/lava-dispatcher/lava-dispatcher.conf
 fi
-# Create users
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('kernel-ci', 'admin@localhost.com', 'kernel-ci')" | lava-server manage shell
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('agl-jenkins', 'admin@localhost.com', 'agl4lava')" | lava-server manage shell
+
+# Create admin users
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('khilman', 'admin@localhost.com', 'lava4me')" | lava-server manage shell
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('jbrunet', 'admin@localhost.com', 'lava4me')" | lava-server manage shell
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('ptitiano', 'admin@localhost.com', 'lava4me')" | lava-server manage shell
 
-# Set the kernelci user's API token
-if [[ -n "$LAVA_API_TOKEN" ]]; then
-	lava-server manage tokens add --user kernel-ci --secret $LAVA_API_TOKEN
-else
-	lava-server manage tokens add --user kernel-ci
-fi
-# Set the kernelci user's API token
-if [[ -n "$AGL_API_TOKEN" ]]; then
-	lava-server manage tokens add --user agl-jenkins --secret $AGL_API_TOKEN
-else
-	lava-server manage tokens add --user agl-jenkins
-fi
+# Add users and tokens from /tmp/tokens
+for file in $(ls /tmp/tokens/*); do
+    user=$(basename $file)
+    token=$(cat $file)
+    pwd=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+    echo "Adding username $user with token $token"
+    echo "from django.contrib.auth.models import User; User.objects.create_superuser('$user', 'admin@localhost.com', '$pwd')" | lava-server manage shell
+    lava-server manage tokens add --user $user --secret $token
+    rm -f $file
+done
 
 # By default add a worker on the master
 lava-server manage workers add $(hostname)
