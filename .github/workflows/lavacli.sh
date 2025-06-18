6cc3ced5faf62d622e35b3c4f0cb99505586fd95
Corentin LABBE <clabbe@baylibre.com>
2025-06-18 08:41:50 +0000

    Test healthcheck image
    THe CI do not verify health checks are okay.
    So we need to verify after docker up that qemu is okay
    
    Signed-off-by: Corentin LABBE <clabbe@baylibre.com>

--- .github/workflows/lavacli.sh
+++ .github/workflows/lavacli.sh
@@ -2,5 +2,19 @@
 
 . output/.env
 
+# verify all devices and jobs are ok
+if [ "$1" = "health" ];then
+	FTMP='/tmp/jobs'
+	lavacli --uri http://$USER:$TOKEN@127.0.0.1:$WEBIF_PORT/RPC2 jobs show $2 > $FTMP
+	cat $FTMP
+	lavacli --uri http://$USER:$TOKEN@127.0.0.1:$WEBIF_PORT/RPC2 jobs logs $2
+	grep 'state.*Finished' $FTMP || exit 1
+	grep 'Health.*Complete' $FTMP || exit 1
+	lavacli --uri http://$USER:$TOKEN@127.0.0.1:$WEBIF_PORT/RPC2 devices show qemu-01 > $FTMP
+	cat $FTMP
+	grep 'health.*Good' $FTMP || exit 1
+	exit 0
+fi
+
 lavacli --uri http://$USER:$TOKEN@127.0.0.1:$WEBIF_PORT/RPC2 $*
 exit $?
--- .github/workflows/wait-for-docker.sh
+++ .github/workflows/wait-for-docker.sh
@@ -14,24 +14,40 @@ cd output/local
 
 TIMEOUT=0
 
+need_wait() {
+	echo "========================================================"
+	lavacli --uri http://admin:tokenforci@127.0.0.1:$WEBIF_PORT/RPC2 devices list > devices.list
+	if [ $? -ne 0 ];then
+		echo "DEBUG: lavacli devices list not0"
+		return 1
+	fi
+	grep -q qemu devices.list
+	if [ $? -ne 0 ];then
+		echo "DEBUG: no qemu yet"
+		cat devices.list
+		return 1
+	fi
+	grep -i unknow devices.list
+	if [ $? -eq 0 ];then
+		echo "DEBUG: there is still devices without passed HC"
+		return 1
+	fi
+	# now wait for a job
+	lavacli --uri http://admin:tokenforci@127.0.0.1:$WEBIF_PORT/RPC2 jobs list > joblist
+	cat joblist
+	grep -q Running joblist
+	if [ $? -ne 0 ];then
+		return 0
+		lavacli --uri http://admin:tokenforci@127.0.0.1:$WEBIF_PORT/RPC2 jobs logs --no-follow 1
+	fi
+	echo "DEBUG: Still running jobs"
+	return 1
+}
+
 while [ $TIMEOUT -le 1200 ]
 do
-	lavacli --uri http://admin:tokenforci@127.0.0.1:$WEBIF_PORT/RPC2 devices list > devices.list
-	if [ $? -eq 0 ];then
-		grep -q qemu devices.list
-		if [ $? -eq 0 ];then
-			lavacli --uri http://admin:tokenforci@127.0.0.1:$WEBIF_PORT/RPC2 devices list
-			# now wait for a job
-			lavacli --uri http://admin:tokenforci@127.0.0.1:$WEBIF_PORT/RPC2 jobs list > joblist
-			grep -q Running joblist
-			if [ $? -eq 0 ];then
-				exit 0
-				lavacli --uri http://admin:tokenforci@127.0.0.1:$WEBIF_PORT/RPC2 jobs logs --no-follow 1
-			else
-				cat joblist
-			fi
-		fi
-	fi
+	need_wait
+	RET=$?
 	docker compose logs --tail=60
 	docker ps > /tmp/alldocker
 	grep -q $DOCKERNAME /tmp/alldocker
@@ -44,7 +60,11 @@ do
 		docker compose logs
 		exit 1
 	fi
+	if [ $RET -eq 0 ];then
+		exit 0
+	fi
 	sleep 10
 	TIMEOUT=$((TIMEOUT+10))
+	echo "WAIT FOR DOCKER TIMEOUT=$TIMEOUT"
 done
 exit 1
